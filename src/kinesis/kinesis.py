bc39da418368a44e8df23de32cffe81653c52c0c
colestanfield <cole.stanfield>
2023-04-29 17:20:50 -0700

    added kinesis streaming

---
+++ src/kinesis/kinesis.py
@@ -0,0 +1,63 @@
+import os
+import boto3
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class KinesisClient:
+
+    def __init__(self):
+        self.kinesis_client = self.get_auth_client("kinesis")
+
+    # See options for authenticating here: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html#credentials
+    def get_auth_client(self, service_name):
+        session = boto3.Session(
+            aws_access_key_id=os.environ["ACCESS_KEY_ID"],
+            aws_secret_access_key=os.environ["ACCESS_KEY_SECRET"],
+        )
+        return session.client(
+            service_name,
+            region_name=os.environ["AWS_REGION"],
+        )
+
+    def record_data_bytes(self, counter):
+        data = {
+            "id": counter,
+            "name": f"Record # {counter}",
+            "test": "test value"
+        }
+        return json.dumps(data).encode("utf-8")
+
+    def generate_record(self, counter, sequence_ordering=None):
+        record = {
+            "Data": self.record_data_bytes(counter),
+            "PartitionKey": "Beta",
+        }
+
+        if sequence_ordering is not None:
+            record["SequenceNumberForOrdering"] = sequence_ordering
+
+        return record
+
+    def put_record(self, record):
+        record_result = self.kinesis_client.put_record(
+            StreamName=os.environ["STREAM_NAME"],
+            Data=record["Data"],
+            PartitionKey=record["PartitionKey"],
+            # SequenceNumberForOrdering=record.get(
+            #     "SequenceNumberForOrdering",
+            #      None,
+            # ),
+            # StreamARN=os.environ["STREAM_ARN"]
+        )
+        print(f"Wrote records: {record_result}")
+
+    def put_records(self, records):
+        records_result = self.kinesis_client.put_records(
+            Records=records,
+            StreamName=os.environ["STREAM_NAME"],
+            # StreamARN=os.environ["STREAM_ARN"]
+        )
+        print(f"Wrote records: {records_result}")
--- src/main.py
+++ src/main.py
@@ -1,24 +1,23 @@
-# import threading
-#
-# from src.sensor.tactilus import PressureSensor
-#
-# if __name__ == "__main__":
-#     thread = PressureSensor(file_count=60)
-#     thread.start()
 import json
+import os
+
 import requests
 from datetime import datetime
 from sseclient import SSEClient
 import threading
+from kinesis import KinesisClient
 
 is_present = True  # set the default value of is_present to True
 frame_id = None  # initialize global variable "id" to None
-frequency = 18000  # framerate in frames per hour. 5 frames per second
+sensor_url = os.environ["SENSOR_URL"]
+frequency = int(os.environ["SENSOR_FREQUENCY"])
+
+kinesis_client = KinesisClient()
 
 
 def start_api_sse_client():
     global frame_id  # use the global keyword to access and modify the global variable
-    url = "http://10.0.0.1/api/sse"
+    url = f"{sensor_url}/api/sse"
     sse = SSEClient(url)
     for response in sse:
         data = response.data.strip()
@@ -33,17 +32,24 @@ def start_api_sse_client():
 
 def start_api_monitor_sse_client():
     global is_present  # use the global keyword to access and modify the global variable
-    url = "http://10.0.0.1/api/monitor/sse"
+    global frame_id
+    url = f"{sensor_url}/api/monitor/sse"
     sse = SSEClient(url)
     for response in sse:
+        print("EVENT: ", response.event)
         if response.event == 'body':
             data = response.data.strip()
             present_field = json.loads(data)['present']
-            if is_present and not present_field:    # indicates a user was in the bed and exited.
+            if is_present and not present_field:  # indicates a user was in the bed and exited.
                 frames = get_frames_within_window()  # get past 300 frames
                 formatted_data = format_sensor_data(frames)  # format data for storage
+                kinesis_client.put_records(formatted_data)
+                print(formatted_data)
             is_present = present_field  # set the value of is_present to present_field
             print(is_present)
+        if response.event == 'newframe':
+            data = response.data.strip()
+            frame_id = json.loads(data)['id']  # percent of storage used
         if response.event == 'storage':
             data = response.data.strip()
             storage_field = json.loads(data)['used']  # percent of storage used
@@ -52,19 +58,21 @@ def start_api_monitor_sse_client():
 
 
 def set_frequency(frequency):
-    url = "http://10.0.0.1/api/frequency"
+    url = f"{sensor_url}/api/frequency"
+    url2 = f"{sensor_url}/api/monitor/storage/frequency"
     headers = {"Content-Type": "application/json"}
-    payload = {"frequency": frequency}
-    response = requests.put(url, headers=headers, json=payload)
+    payload = frequency
+    response1 = requests.put(url, headers=headers, json=payload)
+    response2 = requests.put(url2, headers=headers, json=payload)
 
-    if response.status_code == 204:
+    if response1.status_code == 204 and response2.status_code == 204:
         return True
     else:
         return False  # or raise an exception, depending on your requirements
 
 
 def delete_all_frames():
-    url = "http://10.0.0.1/api/monitor/frames"
+    url = f"{sensor_url}/api/monitor/frames"
     response = requests.delete(url)
     if response.status_code == 204:
         print("All frames deleted successfully.")
@@ -77,17 +85,16 @@ def get_frames_within_window():
         return None  # or raise an exception, depending on your requirements
 
     if frame_id < 300:
-        before_frame = 0
+        after_frame = 0
     else:
-        before_frame = frame_id - 300
+        after_frame = frame_id - 2
+    before_frame = frame_id
 
-    after_frame = frame_id
-
-    url = f"http://10.0.0.1/api/monitor/frames?before={before_frame}&after={after_frame}"
+    url = f"{sensor_url}/api/monitor/frames?after={after_frame}&before={before_frame}"
     response = requests.get(url)
 
     if response.status_code == 200:
-        frames = response.json()
+        frames = json.loads(response.text)
         return frames
     else:
         return None  # or raise an exception, depending on your requirements
@@ -100,16 +107,30 @@ def format_sensor_data(readings):
     data = {
         "id": frame_id,
         "time": timestamp,
+        "patient_present": is_present,
         "frequency": frequency,
-        "readings": readings
+        "readings": format_readings(readings)
     }
     json_data = json.dumps(data)
     return json_data
 
 
+def format_readings(readings):
+    output_array = []
+    for i, obj in enumerate(readings):
+        output_obj = {
+            "frame": i,
+            "readings": obj["readings"][0]
+        }
+        output_array.append(output_obj)
+    return output_array
+
+
 if __name__ == '__main__':
-    thread1 = threading.Thread(target=start_api_monitor_sse_client)
-    thread1.start()
-    thread2 = threading.Thread(target=start_api_sse_client)
-    thread2.start()
+    # kinesis_client = KinesisClient()
+    # kinesis_client.put_records([kinesis_client.generate_record(1)])
+    set_frequency(frequency)
+    start_api_monitor_sse_client()
+    # thread1 = threading.Thread(target=start_api_monitor_sse_client)
+    # thread1.start()
     print("API SSE Client started on separate threads.")
