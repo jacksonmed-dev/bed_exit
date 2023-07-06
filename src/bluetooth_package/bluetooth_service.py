bef62dbd4d0ae55ab0d748699809e3cbf2a532f5
colestanfield <cole.stanfield>
2023-07-06 08:35:29 -0700

    ble

--- src/bluetooth_package/bluetooth_service.py
+++ src/bluetooth_package/bluetooth_service.py
@@ -1,28 +1,270 @@
-import bluetooth
+#!/usr/bin/env python3
+
+import logging
+
+import dbus
+import dbus.exceptions
+import dbus.mainloop.glib
+import dbus.service
+
+from ble import (
+    Advertisement,
+    Characteristic,
+    Service,
+    Application,
+    find_adapter,
+    Descriptor,
+    Agent,
+)
+
+import struct
+import requests
+import array
+from enum import Enum
+
+import sys
+
+MainLoop = None
+try:
+    from gi.repository import GLib
+
+    MainLoop = GLib.MainLoop
+except ImportError:
+    import gobject as GObject
+
+    MainLoop = GObject.MainLoop
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logHandler = logging.StreamHandler()
+filelogHandler = logging.FileHandler("logs.log")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logHandler.setFormatter(formatter)
+filelogHandler.setFormatter(formatter)
+logger.addHandler(filelogHandler)
+logger.addHandler(logHandler)
+
+mainloop = None
+
+BLUEZ_SERVICE_NAME = "org.bluez"
+GATT_MANAGER_IFACE = "org.bluez.GattManager1"
+LE_ADVERTISEMENT_IFACE = "org.bluez.LEAdvertisement1"
+LE_ADVERTISING_MANAGER_IFACE = "org.bluez.LEAdvertisingManager1"
 
 
-class BluetoothService:
-    def __init__(self):
-        self.server_socket = None
-        self.client_socket = None
+class InvalidArgsException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
 
-    def start_service(self):
-        self.server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self.server_socket.bind(("", bluetooth.PORT_ANY))
-        self.server_socket.listen(1)
-        port = self.server_socket.getsockname()[1]
-        print("Service started. Listening on port", port)
 
-        self.client_socket, client_address = self.server_socket.accept()
-        print("Accepted connection from", client_address)
+class NotSupportedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotSupported"
 
-    def stop_service(self):
-        if self.client_socket:
-            self.client_socket.close()
-        if self.server_socket:
-            self.server_socket.close()
-        print("Service stopped")
 
-    def receive_data(self):
-        data = self.client_socket.recv(1024)
-        print("Received:", data.decode())
+class NotPermittedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotPermitted"
+
+
+class InvalidValueLengthException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.InvalidValueLength"
+
+
+class FailedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.Failed"
+
+
+def register_app_cb():
+    logger.info("GATT application registered")
+
+
+def register_app_error_cb(error):
+    logger.critical("Failed to register application: " + str(error))
+    mainloop.quit()
+
+
+class JXNS1Service(Service):
+    """
+    Dummy test service that provides characteristics and descriptors that
+    exercise various API functionality.
+
+    """
+
+    WIFI_SVC_UUID = "12634d89-d598-4874-8e86-7d042ee07ba7"
+
+    def __init__(self, bus, index):
+        Service.__init__(self, bus, index, self.WIFI_SVC_UUID, True)
+        self.add_characteristic(WifiPasswordCharacteristic(bus, 0, self))
+        # self.add_characteristic(BoilerControlCharacteristic(bus, 1, self))
+        # self.add_characteristic(AutoOffCharacteristic(bus, 2, self))
+
+
+class WifiPasswordCharacteristic(Characteristic):
+    uuid = "4116f8d2-9f66-4f58-a53d-fc7440e7c14e"
+    description = b"Get/set machine power state {'ON', 'OFF', 'UNKNOWN'}"
+
+    class State(Enum):
+        on = "ON"
+        off = "OFF"
+        unknown = "UNKNOWN"
+
+        @classmethod
+        def has_value(cls, value):
+            return value in cls._value2member_map_
+
+    power_options = {"ON", "OFF", "UNKNOWN"}
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index, self.uuid, ["encrypt-read", "encrypt-write"], service,
+        )
+
+        self.value = [0xFF]
+        self.add_descriptor(CharacteristicUserDescriptionDescriptor(bus, 1, self))
+
+    def ReadValue(self, options):
+        logger.debug("power Read: " + repr(self.value))
+        res = None
+        try:
+            # res = requests.get(JXNBaseUrl + "/sensor")
+            self.value = bytearray("Setting your password complete", encoding="utf8")
+        except Exception as e:
+            logger.error(f"Error getting status {e}")
+            self.value = bytearray(self.State.unknown, encoding="utf8")
+
+        return self.value
+
+    def WriteValue(self, value, options):
+        logger.debug("power Write: " + repr(value))
+        cmd = bytes(value).decode("utf-8")
+        logger.info(f"receiving command: {cmd}")
+        if self.State.has_value(cmd):
+            # write it to machine
+            logger.info("writing {cmd} to machine")
+            data = {"cmd": cmd.lower()}
+            try:
+                logger.info(f"state written {cmd}")
+                # res = requests.post(JXNBaseUrl + "/sensor/cmds", json=data)
+            except Exceptions as e:
+                logger.error(f"Error updating machine state: {e}")
+        else:
+            logger.info(f"invalid state written {cmd}")
+            raise NotPermittedException
+
+        self.value = value
+
+
+class CharacteristicUserDescriptionDescriptor(Descriptor):
+    """
+    Writable CUD descriptor.
+    """
+    CUD_UUID = "2901"
+
+    def __init__(
+            self, bus, index, characteristic,
+    ):
+        self.value = array.array("B", characteristic.description)
+        self.value = self.value.tolist()
+        Descriptor.__init__(self, bus, index, self.CUD_UUID, ["read"], characteristic)
+
+    def ReadValue(self, options):
+        return self.value
+
+    def WriteValue(self, value, options):
+        if not self.writable:
+            raise NotPermittedException()
+        self.value = value
+
+
+class JXNAdvertisement(Advertisement):
+    def __init__(self, bus, index):
+        Advertisement.__init__(self, bus, index, "peripheral")
+        self.add_manufacturer_data(
+            0xFFFF, [0x70, 0x74],
+        )
+        self.add_service_uuid(JXNS1Service.WIFI_SVC_UUID)
+
+        self.add_local_name("JXN")
+        self.include_tx_power = True
+
+
+def register_ad_cb():
+    logger.info("Advertisement registered")
+
+
+def register_ad_error_cb(error):
+    logger.critical("Failed to register advertisement: " + str(error))
+    mainloop.quit()
+
+
+AGENT_PATH = "/com/punchthrough/agent"
+
+class JXNBluetoothService() :
+    def __int__(self):
+        self.bus = dbus.SystemBus()
+        self.adapter = find_adapter(self.bus)
+        if not self.adapter:
+            logger.critical("GattManager1 interface not found")
+            return
+
+def main():
+    global mainloop
+
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    # get the system bus
+    bus = dbus.SystemBus()
+    # get the ble controller
+    adapter = find_adapter(bus)
+
+    if not adapter:
+        logger.critical("GattManager1 interface not found")
+        return
+
+    adapter_obj = bus.get_object(BLUEZ_SERVICE_NAME, adapter)
+
+    adapter_props = dbus.Interface(adapter_obj, "org.freedesktop.DBus.Properties")
+
+    # powered property on the controller to on
+    adapter_props.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
+
+    # Get manager objs
+    service_manager = dbus.Interface(adapter_obj, GATT_MANAGER_IFACE)
+    ad_manager = dbus.Interface(adapter_obj, LE_ADVERTISING_MANAGER_IFACE)
+
+    advertisement = JXNAdvertisement(bus, 0)
+    obj = bus.get_object(BLUEZ_SERVICE_NAME, "/org/bluez")
+
+    agent = Agent(bus, AGENT_PATH)
+
+    app = Application(bus)
+    app.add_service(JXNS1Service(bus, 2))
+
+    mainloop = MainLoop()
+
+    agent_manager = dbus.Interface(obj, "org.bluez.AgentManager1")
+    agent_manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput")
+
+    ad_manager.RegisterAdvertisement(
+        advertisement.get_path(),
+        {},
+        reply_handler=register_ad_cb,
+        error_handler=register_ad_error_cb,
+    )
+
+    logger.info("Registering GATT application...")
+
+    service_manager.RegisterApplication(
+        app.get_path(),
+        {},
+        reply_handler=register_app_cb,
+        error_handler=[register_app_error_cb],
+    )
+
+    agent_manager.RequestDefaultAgent(AGENT_PATH)
+
+    mainloop.run()
+    # ad_manager.UnregisterAdvertisement(advertisement)
+    # dbus.service.Object.remove_from_connection(advertisement)
+
+
+if __name__ == "__main__":
+    main()
