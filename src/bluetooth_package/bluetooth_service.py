e356a0d159728c9902b86f6ed364afde2c9917d6
colestanfield <cole.stanfield>
2023-07-10 22:00:54 -0700

    fixed imports

--- src/bluetooth_package/bluetooth_service.py
+++ src/bluetooth_package/bluetooth_service.py
@@ -19,8 +19,7 @@ import dbus.service
 import array
 from enum import Enum
 
-from src.bluetooth_package.ble import find_adapter, Advertisement, Descriptor, Characteristic, Service, Agent, \
-    Application
+from ble import find_adapter, Advertisement, Descriptor, Characteristic, Service, Agent, Application
 
 MainLoop = None
 try:
