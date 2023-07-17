f6cc4c1881d402f04c4522d619e74a461a6d5dc4
colestanfield <cole.stanfield>
2023-07-16 18:32:50 -0700

    test turntimer

--- src/main.py
+++ src/main.py
@@ -76,7 +76,7 @@ class BedExitMonitor:
             if not is_ok:
                 print("Calling backend")
                 self.kinesis_client.signed_request_v2(os.environ["EVENT_ENDPOINT"],
-                                                      {"eventType": "bedExit", "sensorId": os.environ["SENSOR_SSID"]})
+                                                      {"eventType": "turnTimerExpire", "sensorId": os.environ["SENSOR_SSID"]})
                 reset_rotation_interval()
         else:
             reset_rotation_interval()
