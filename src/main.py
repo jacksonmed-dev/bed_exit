3500c17b46ff71edd70bee55257425fc2b957888
colestanfield <cole.stanfield>
2023-07-16 17:19:11 -0700

    starting the service

--- src/main.py
+++ src/main.py
@@ -199,7 +199,7 @@ class BedExitMonitor:
 
 if __name__ == '__main__':
     service = BedExitMonitor()
-    service.start()
+    # service.start()
     service.start_api_monitor_sse_client()
     # disconnect_from_wifi_network(wireless_interface="wlan1")
     # connect_to_wifi_network(
