0c363cac25f9833d8f5a79c06e8221ed2130eb0f
colestanfield <cole.stanfield>
2023-07-16 18:04:43 -0700

    printing attended event

--- src/main.py
+++ src/main.py
@@ -11,91 +11,6 @@ from sensor import get_frames_within_window, format_sensor_data, delete_all_fram
 from wifi import connect_to_wifi_network
 
 
-# is_present = False  # set the default value of is_present to True
-# is_sensor_present = False
-#
-# is_timer_enabled = False
-# timer_thread = None
-#
-# frame_id = None  # initialize global variable "id" to None
-# sensor_url = os.environ["SENSOR_URL"]
-
-
-# def start_api_monitor_sse_client(kinesis_client):
-#     global is_present, is_sensor_present, is_timer_enabled  # use the global keyword to access and modify the global variable
-#     global frame_id
-#     url = f"{sensor_url}/api/monitor/sse"
-#     sse = SSEClient(url)
-#     for response in sse:
-#         data = response.data.strip()
-#         if response.event == 'body':
-#             handle_body_event(data, kinesis_client)
-#         if response.event == 'newframe':
-#             handle_new_frame_event(data)
-#         if response.event == 'storage':
-#             handle_storage_event(data)
-#
-#
-# # Function to update the is_patient_present variable
-# def update_patient_presence():
-#     global is_present, is_sensor_present, is_timer_enabled
-#     print(f"Updating pi is_present to {is_sensor_present}")
-#     is_present = is_sensor_present
-#     is_timer_enabled = False
-#
-#
-# def run_update_patient_presence():
-#     global is_timer_enabled, timer_thread
-#     print("Starting new timer...")
-#     is_timer_enabled = True
-#     timer_thread = threading.Timer(60, update_patient_presence)
-#     timer_thread.start()
-#
-#
-# def handle_body_event(data, kinesis_client):
-#     print("############## BODY EVENT ###############")
-#     global is_present, is_sensor_present
-#     is_sensor_present = json.loads(data)['present']
-#     print(f"sensor patient present:\t{is_sensor_present}")
-#     print(f"pi patient present:\t{is_present}")
-#
-#     if is_timer_enabled and timer_thread is not None:
-#         print("Cancelling Timer... Body event detected before timer expired")
-#         timer_thread.cancel()
-#         run_update_patient_presence()  # Run this function on a separate non blocking thread
-#     if is_present and not is_sensor_present and not is_timer_enabled:  # indicates a user was in the bed and exited.
-#         print("--- PATIENT EXIT DETECTED ---")
-#         run_update_patient_presence()  # Run this function on a separate non blocking thread
-#         frames = get_frames_within_window(frame_id)  # get past 300 frames
-#         formatted_data = format_sensor_data(frames, is_present, frequency=int(
-#             os.environ["SENSOR_FREQUENCY"]))  # format data for storage
-#         kinesis_client.put_records(formatted_data)
-#     if not is_present and is_sensor_present and not is_timer_enabled:
-#         print("--- PATIENT ENTRY DETECTED ---")
-#         run_update_patient_presence()
-#     if not is_timer_enabled:
-#         is_present = is_sensor_present
-#     print("\n\n")
-#
-#
-# def handle_new_frame_event(data):
-#     global frame_id
-#     frame_id = json.loads(data)['id']  # percent of storage used
-#
-#
-# def handle_storage_event(data):
-#     print("############## STORAGE EVENT ###############")
-#     storage_field = json.loads(data)['used']  # percent of storage used
-#     print(f"Storage Used: {storage_field}%")
-#     if storage_field > 85:
-#         delete_all_frames()
-#     print("\n\n")
-#
-#
-# def set_wifi(new_password):
-#     print("HELLO I THINK I GOT IT WORKING!!!!!")
-#     connect_to_wifi_network(network_ssid="Stanfield Wifi", network_password=new_password, wireless_interface="wlan1")
-
 class BedExitMonitor:
     def __init__(self):
         self.is_present = False
@@ -129,7 +44,8 @@ class BedExitMonitor:
             self.stop_api_monitor_sse_client()
         elif input_array[0] == "wifi":
             print("initializing sensor wifi connection")
-            connect_to_wifi_network(network_ssid="DataPort-BT2-2764103-000107", network_password="boditr@k", wireless_interface="wlan1")
+            connect_to_wifi_network(network_ssid="DataPort-BT2-2764103-000107", network_password="boditr@k",
+                                    wireless_interface="wlan1")
 
     def start_api_monitor_sse_client(self):
         print("starting the sse client")
@@ -137,7 +53,9 @@ class BedExitMonitor:
         self.sse_client = SSEClient(url)
         for response in self.sse_client:
             data = response.data.strip()
-            print("### {} EVENT ####".format(response.event))
+            if response.event == "attended":
+                print("### {} EVENT ####".format(response.event))
+                print(data)
             # if response.event == 'body':
             #     print("############## BODY EVENT ###############")
             #     self.handle_body_event(data)
@@ -166,7 +84,7 @@ class BedExitMonitor:
             print("--- PATIENT EXIT DETECTED ---")
             print("---- SENDING EXIT EVENT -----")
             self.kinesis_client.signed_request_v2(os.environ["EVENT_ENDPOINT"],
-                {"eventType": "bedExit", "sensorId": os.environ["SENSOR_SSID"]})
+                                                  {"eventType": "bedExit", "sensorId": os.environ["SENSOR_SSID"]})
             self.run_update_patient_presence()
             frames = get_frames_within_window(self.frame_id)
             formatted_data = format_sensor_data(frames, self.is_present, frequency=int(os.environ["SENSOR_FREQUENCY"]))
@@ -205,6 +123,7 @@ class BedExitMonitor:
     def start(self):
         self.bluetooth_service.start()
         # self.start_api_monitor_sse_client()
+
 
 if __name__ == '__main__':
     service = BedExitMonitor()
