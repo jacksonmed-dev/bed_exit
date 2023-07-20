695295ac2706f0bae6b412795dbaa9b653b693c9
colestanfield <cole.stanfield>
2023-07-20 16:31:21 -0700

    quick log

--- src/main.py
+++ src/main.py
@@ -81,7 +81,6 @@ class BedExitMonitor:
         self.sse_client = SSEClient(url)
         for response in self.sse_client:
             data = response.data.strip()
-            logger.info(response.event)
             if response.event == "attended":
                 self.handle_attended_event(data)
             if response.event == 'body':
