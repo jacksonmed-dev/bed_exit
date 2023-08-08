15ece6b32b17cde98894c1d737512ce1f21766cf
colestanfield <cole.stanfield>
2023-08-08 08:44:34 -0700

    import fix

--- src/lcd_display/lcd.py
+++ src/lcd_display/lcd.py
@@ -1,7 +1,9 @@
+import time
 from signal import signal, SIGTERM, SIGHUP, pause
 from rpi_lcd import LCD
 
 lcd = LCD()
+
 
 def display_message(message):
     def safe_exit(signum, frame):
@@ -26,6 +28,7 @@ def display_message(message):
 def safe_exit(signum, frame):
     exit(1)
 
+
 def scroll_text(text, line, speed=0.5):
     text = text + " " * 16  # Add padding to the end of the text
     text_length = len(text)
@@ -36,12 +39,12 @@ def scroll_text(text, line, speed=0.5):
 
         while True:
             for i in range(text_length - 16 + 1):
-                lcd.text(text[i:i+16], line)
-                pause(speed)
+                lcd.text(text[i:i + 16], line)
+                time.sleep(speed)
+
 
     except KeyboardInterrupt:
         pass
 
     finally:
         lcd.clear()
-
