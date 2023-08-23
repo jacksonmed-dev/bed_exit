956994d211f25a2d1186e380af0a8437bbafdb6a
colestanfield <cole.stanfield>
2023-08-23 15:15:24 -0700

    best implementation so far

--- src/lcd_display/lcd.py
+++ src/lcd_display/lcd.py
@@ -9,39 +9,42 @@ lcd = LCD()
 
 class ScrollingText:
     def __init__(self):
-        self._threads = [None] * 2
-        self._lines = ["", ""]
+        self._thread = None
+        self._line1 = ""
+        self._line2 = ""
         self._stop_event = threading.Event()
 
-    def _scroll_thread(self, text, line):
+    def _scroll_thread(self):
+        while not self._stop_event.is_set():
+            if self._line1 or self._line2:
+                self._scroll_line(self._line1, 1)
+                self._scroll_line(self._line2, 2)
+
+    def _scroll_line(self, text, line):
+        if not text:
+            return
+
         text = text + " " * 16
         text_length = len(text)
 
-        while not self._stop_event.is_set():
-            for i in range(text_length - 16 + 1):
-                if self._stop_event.is_set():
-                    break
-                lcd.text(text[i:i + 16], line)
-                time.sleep(0.1)  # Adjust the sleep duration for scrolling speed
+        for i in range(text_length - 16 + 1):
+            if self._stop_event.is_set():
+                break
+            lcd.text(text[i:i + 16], line)
+            time.sleep(0.1)  # Adjust the sleep duration for scrolling speed
 
     def set_lines(self, line1, line2):
-        self._lines[0] = line1
-        self._lines[1] = line2
-        self._stop_event.clear()
-
-        if self._threads[0] is None or not self._threads[0].is_alive():
-            self._threads[0] = threading.Thread(target=self._scroll_thread, args=(line1, 1))
-            self._threads[0].start()
-
-        if self._threads[1] is None or not self._threads[1].is_alive():
-            self._threads[1] = threading.Thread(target=self._scroll_thread, args=(line2, 2))
-            self._threads[1].start()
+        self._line1 = line1
+        self._line2 = line2
+        if self._thread is None or not self._thread.is_alive():
+            self._stop_event.clear()
+            self._thread = threading.Thread(target=self._scroll_thread)
+            self._thread.start()
 
     def stop(self):
         self._stop_event.set()
-        for thread in self._threads:
-            if thread is not None:
-                thread.join()
+        if self._thread is not None:
+            self._thread.join()
 
 
 def safe_exit(signum, frame):
