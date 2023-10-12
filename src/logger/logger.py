d471bc01b86969938f2fc3ec8c89913226c0913f
colestanfield <cole.stanfield>
2023-10-11 17:41:58 -0700

    refactoring the logger

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,8 +1,19 @@
 import logging
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Logger configuration")
+parser.add_argument(
+    "--log-level",
+    choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    default="DEBUG",
+    help="Set the log level (default: DEBUG)",
+)
+args = parser.parse_args()
+log_level = getattr(logging, args.log_level)
 
 # Configure the logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 logHandler = logging.StreamHandler()
 filelogHandler = logging.FileHandler("logs.log")
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -10,3 +21,4 @@ logHandler.setFormatter(formatter)
 filelogHandler.setFormatter(formatter)
 logger.addHandler(filelogHandler)
 logger.addHandler(logHandler)
+logger.setLevel(log_level)
