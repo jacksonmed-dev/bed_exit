7f63ffcf8869a90bd7bbc9c06b28a436b9eda072
colestanfield <cole.stanfield>
2023-10-11 17:25:16 -0700

    fixing imports

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -2,6 +2,7 @@ import logging
 
 # Configure the logger
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 logHandler = logging.StreamHandler()
 filelogHandler = logging.FileHandler("logs.log")
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
