068f302d1d533290effbf5e4e0799bfa46bb371d
colestanfield <cole.stanfield>
2023-10-11 18:10:39 -0700

    testing startup script changes

--- setup_run.bash
+++ setup_run.bash
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
+# List of packages to install
+required_packages=("python3-dbus" "libglib2.0-dev" "libgirepository1.0-dev" "libcairo2-dev" "python3-venv")
 
-source ./venv/bin/activate
+# Check and install missing packages
+for package in "${required_packages[@]}"; do
+    if ! dpkg -l | grep -q "^ii  $package"; then
+        echo "Installing $package..."
+        sudo apt install -y "$package"
+    else
+        echo "$package is already installed."
+    fi
+done
 
-sudo apt install python3-dbus
+# Check if the venv directory exists
+if [ ! -d "venv" ]; then
+    echo "Creating Python virtual environment..."
+    python3 -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
 
 pip3 install -r requirements.txt
 
